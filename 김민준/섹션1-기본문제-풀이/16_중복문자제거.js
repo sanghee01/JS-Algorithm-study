71ec44e66c705c5ea51795646678218b8dd9b50c
mj0107 <alswns1285@gmail.com>
2022-12-31 23:35:07 +0900

    :hammer: 16. 중복문자제거
    - indexOf를 활용한 풀이법 추가

--- 김민준/섹션1-기본문제-풀이/16_중복문자제거.js
+++ 김민준/섹션1-기본문제-풀이/16_중복문자제거.js
@@ -1,4 +1,14 @@
 function solution(str) {
+  let answer = '';
+
+  for(let i=0; i<str.length; i+=1) {
+    if(str.indexOf(str[i]) === i) answer += str[i];
+  }
+
+  return answer;
+}
+
+function solution2(str) {
   let set = new Set(str);
   let answer = [...set].join('');
 
