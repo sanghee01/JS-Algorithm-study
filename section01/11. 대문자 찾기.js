0aa3796f41d1cf499414e360a2ab73f7c506d88b
km6293 <69450660+km6293@users.noreply.github.com>
2023-01-02 20:47:09 +0900

    Delete section01 directory

--- section01/11. 대문자 찾기.js
+++
@@ -1,9 +0,0 @@
-function solution(str,a){
-  let cnt = 0;
-  for(let i = 0; i < str.length; i++){
-    if(str[i] === str[i].toUpperCase())cnt += 1;
-  }
-  return cnt;
-}
-
-console.log(solution('KoreaTimeGood'),3)
--- section01/9. A를 #으로.js
+++
@@ -1,6 +0,0 @@
-function solution(str){
-  return str.replace(/A/g,'#');
-}
-
-console.log(solution('BANANA'),'B#N#N#')
-console.log(solution('AAAAAA'),'B#N#N#')
