080633895eb42def359c72797c505461120954f2
km6293 <69450660+km6293@users.noreply.github.com>
2023-01-04 16:23:03 +0900

    Update 13. 대소문자 변환.js

--- 김민수/section01/13. 대소문자 변환.js
+++ 김민수/section01/13. 대소문자 변환.js
@@ -1,5 +1,9 @@
 function solution(str){
-  return str.toUpperCase();
+  let val = '';
+  for(let i = 0; i < str.length; i++){
+    str[i] === str[i].toLowerCase() ? val += str[i].toUpperCase() : val += str[i].toLowerCase();
+  }
+  return val
 }
 
-console.log(solution('ItisTimeToStudy'),'ITISTIMETOSTUDY')
+console.log(solution('StuDY'),'sTUdy')
