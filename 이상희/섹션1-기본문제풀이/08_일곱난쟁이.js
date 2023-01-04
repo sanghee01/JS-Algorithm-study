eb72e5f4178e96d4c552053f25e7d85a9ec743da
sanghee01 <sang6299@naver.com>
2023-01-04 15:47:46 +0900

    메서드 잘못 사용

--- 이상희/섹션1-기본문제풀이/08_일곱난쟁이.js
+++ 이상희/섹션1-기본문제풀이/08_일곱난쟁이.js
@@ -6,8 +6,8 @@ function solution(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 1; j < arr.length; j++) {
       if (sum - (answer[i] + answer[j]) === 100) {
-        answer.slice(j, 1);
-        answer.slice(i, 1);
+        answer.splice(j, 1);
+        answer.splice(i, 1);
         break;
       }
     }
