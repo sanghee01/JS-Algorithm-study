8622cf1c2a019c2cc8ea3348a53cbab6c9514f74
km6293 <69450660+km6293@users.noreply.github.com>
2023-01-10 00:05:12 +0900

    Update 01. 큰 수 출력하기.js

--- 김민수/section02/01. 큰 수 출력하기.js
+++ 김민수/section02/01. 큰 수 출력하기.js
@@ -1,10 +1,9 @@
-function solution(a,arr){
-  // 0이 아닌 1부터 a(arr.length)까지
-  for(let i = 1; i < a; i++){
-    // 1과 0 비교, 2와 1 비교, 3과 2 비교.... 후에 splice로 제거
-    if(arr[i-1] >= arr[i]) arr.splice(i,1)
+function solution(arr){
+  let nArr = [arr[0]];
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i-1] < arr[i]) nArr.push(arr[i])
   }
-  return arr
+  return nArr
 }
 
-console.log(solution(6, [7 ,3 ,9 ,5 ,6 ,12]), [7,9,6,12]);
+console.log(solution([7 ,3 ,9 ,5 ,6 ,12]), [7,9,6,12]);
