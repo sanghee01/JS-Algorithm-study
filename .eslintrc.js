9ef12b583532d473740913bae7acacfcd47cf5e3
jihyunlee <jihyunlee.me@gmail.com>
2023-01-11 20:52:46 +0900

    Revert "eslint 수정 - windows환경에서 delete cr 경고 안뜨게 disabled"
    This reverts commit b21d9e75e7cac045e28fc8e08e9a558052a011d4.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
+    "prettier/prettier": ["error"],
     "no-unused-vars": "off",
   },
 };
