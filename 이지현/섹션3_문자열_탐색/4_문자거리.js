138a8e0d3484a547a9261602a4e9ea0b8c8e6531
jihyunlee <jihyunlee.me@gmail.com>
2023-01-18 07:33:46 +0900

    4. 문자거리

---
+++ 이지현/섹션3_문자열_탐색/4_문자거리.js
@@ -0,0 +1,28 @@
+function solution(str, element){
+	let answer = [];
+    let p = 100;
+
+    for (x of str) {
+    	if (x === element){
+        	p = 0;
+            answer.push(p);
+        } else {
+        	p++;
+            answer.push(p);
+        }
+    }
+
+    p = 100;
+    for (let i = str.length - 1; i >= 0;  i--){
+    	if (str[i] === element){
+        	p = 0;
+        } else {
+        	p++;
+            answer[i] = Math.min(answer[i], p);
+        }
+     }
+     return answer;
+}
+  
+let str = "teachermode";
+console.log(solution(str, 'e'));
