2725904393b0f9d0e2b179fee720f4eb8ffea40d
jihyunlee <jihyunlee.me@gmail.com>
2023-01-18 07:33:53 +0900

    5. 문자열 압축

---
+++ 이지현/섹션3_문자열_탐색/5_문자열_압출.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let answer="";
+    let cnt=1;
+    s = s + " ";
+    for (let i = 0; i < s.length - 1; i++) {
+        if ( s[i]===s[i+1]) cnt++;
+        else {
+            answer += s[i];
+            if(cnt > 1) answer += String(cnt);
+            cnt = 1;
+        }
+    }
+    return answer;
+  }
+  
+  console.log(solution("KKHSSSSSSSE"));
