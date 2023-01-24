a4dc3fcc96584c0c038505e2b15c6bf3634650d7
sanghee01 <sang6299@naver.com>
2023-01-24 14:31:20 +0900

    23/01/24 섹션4-3,4

---
+++ 이상희/섹션4-완전탐색(블루투포스)/04_졸업 선물.js
@@ -0,0 +1,29 @@
+// 기준이 없다 -> 다 탐색해야한다 (완전탐색)
+
+function solution(m, product) {
+  let answer = 0;
+  let n = product.length;
+  product.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  for (let i = 0; i < n; i++) {
+    let money = m - (product[i][0] / 2 + product[i][1]);
+    let cnt = 1;
+    for (let j = 0; j < n; j++) {
+      if (j !== i && product[j][0] + product[j][1] > money) break; // n이 엄청 클때 헛바퀴 도는거 방지
+      if (j !== i && product[j][0] + product[j][1] <= money) {
+        money -= product[j][0] + product[j][1];
+        cnt++;
+      }
+    }
+    answer = Math.max(answer, cnt);
+  }
+  return answer;
+}
+
+let arr = [
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
+];
+console.log(solution(28, arr));
