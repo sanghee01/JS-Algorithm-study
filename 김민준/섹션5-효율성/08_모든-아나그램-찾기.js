d34c47a34f8644a693c32b315ba728abf1a28b5f
mj0107 <alswns1285@gmail.com>
2023-01-26 21:49:03 +0900

    :art: 8. 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)
    prettier를 적용안하고 commit 해서 코드 변화는 없고 간단히 들여쓰기 조금 수정 했습니다

--- 김민준/섹션5-효율성/08_모든-아나그램-찾기.js
+++ 김민준/섹션5-효율성/08_모든-아나그램-찾기.js
@@ -1,8 +1,8 @@
 /**
  * T와 부분문자열인 subString을 받아서 T의 아나그램이 되는지 판단해서 boolean 값으로 반환하는 함수
- * 
- * @param {string} T 
- * @param {string} subString 
+ *
+ * @param {string} T
+ * @param {string} subString
  * @returns {boolean} substring이 T의 아나그램이 된다면 true 반환, 아니면 false 반환
  */
 function isAnagram(T, subString) {
@@ -29,12 +29,12 @@ function solution(S, T) {
   let [start, end] = [0, T.length - 1];
 
   let subString = '';
-  while(true) {
-    if(end === S.length) break;
+  while (true) {
+    if (end === S.length) break;
 
     subString = S.slice(start, end + 1);
-    
-    if(isAnagram(T, subString) === true) {
+
+    if (isAnagram(T, subString) === true) {
       cnt += 1;
     }
 
