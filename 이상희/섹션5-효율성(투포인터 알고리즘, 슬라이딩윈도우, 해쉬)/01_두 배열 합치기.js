2a609bf48d99b7ce5d31072c92555ed93de2d710
sanghee01 <sang6299@naver.com>
2023-01-28 22:30:26 +0900

    23/01/28 섹션5-1,2

---
+++ 이상희/섹션5-효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/01_두 배열 합치기.js
@@ -0,0 +1,28 @@
+function solution(arr1, arr2) {
+  let answer = [].concat(arr1, arr2);
+  answer.sort((a, b) => {
+    a - b;
+  }); // sort : O(nlogn)
+
+  return answer;
+}
+
+// 투 포인터식 풀이 : O(n)
+function solution2(arr1, arr2) {
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  let p1 = 0;
+  let p2 = 0;
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+    else answer.push(arr2[p2++]);
+  }
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+  return answer;
+}
+
+let a = [1, 3, 5];
+let b = [2, 3, 6, 7, 9];
+console.log(solution2(a, b));
