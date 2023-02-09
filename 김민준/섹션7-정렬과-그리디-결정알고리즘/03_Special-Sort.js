26b60633e6b3e33e4c7b3f8f7032deb6a3dea2bf
mj0107 <alswns1285@gmail.com>
2023-02-10 05:49:51 +0900

    :sparkles: 3. Special Sort(구글 인터뷰)

---
+++ 김민준/섹션7-정렬과-그리디-결정알고리즘/03_Special-Sort.js
@@ -0,0 +1,15 @@
+function solution(N, NUMBER_LIST) {
+  let result = [];
+  let numberList = [...NUMBER_LIST];
+
+  let negativeNumberList = numberList.filter((num) => num < 0);
+  let positiveNumberList = numberList.filter((num) => num > 0);
+
+  result = [...negativeNumberList, ...positiveNumberList];
+  return result;
+}
+
+const N = 8;
+const NUMBER_LIST = [1,2,3,-3,-2,5,6,-6];
+const ANSWER = solution(N, NUMBER_LIST);
+console.log(ANSWER);
