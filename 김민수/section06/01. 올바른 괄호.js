55dec542771451172214734bea1491be88bbb6b5
km6293 <69450660+km6293@users.noreply.github.com>
2023-02-15 18:35:34 +0900

    Delete 김민수/section06 directory

--- 김민수/section06/01. 올바른 괄호.js
+++
@@ -1,8 +0,0 @@
-function solution(str) {
-  let cnt = 0;
-  for (let i = 0; i < str.length; i++) {
-    str[i] == "(" ? (cnt += 1) : (cnt -= 1);
-  }
-  return cnt == 0 ? "YES" : "NO";
-}
-console.log(solution("(())()"));
